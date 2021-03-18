7ebff8eab9a5d20a6147e002c914833701eb824e
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-03-18 13:56:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,29 +11,7 @@ class School
   end
 
   def add_student(student_name, grade)
-    # @roster[grade] ||= []    or equals operator is ||= it does if/else in one line
-    # @roster[grade] << student_name
-
-    # @roster[grade] checks to see if that exists
-
-    if !@roster[grade]
-      @roster[grade] = []
-      @roster[grade] << student_name
-    else
-      @roster[grade] << student_name
-    end
-
-  end
-
-  def grade(student_name)
-    @roster[student_name]
-  end
-
-  def sort
-    sorted = {}
-    @roster.each do |grade, student|
-      sorted[grade] = student.sort
-    end
-    sorted
+    roster[grade] ||= []
+    roster[grade] << student_name
   end
 end
