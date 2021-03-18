db6e210505419336865dec18425c038a85dc0a68
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-03-18 14:25:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -24,7 +24,7 @@ class School
   def sort
     sorted = {}
     roster.each do |grade, student|
-      sorted = roster[grade] << student.sort
+      sorted[grade] = student.sort
     end
     sorted
   end
